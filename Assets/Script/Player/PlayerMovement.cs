f2411385a3e8b47ab308c4cb5ddf205032e88a7d
Quynh33 <105623183+Quynh33@users.noreply.github.com>
2024-03-22 17:57:43 +0700

    Game Files

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  
+    Animator anim;
+    public static PlayerMovement Instance;
+
+    [Header("Horizontal Movement Setting")]
+    private Rigidbody2D rb;
+    public float speed;
+    private float xAxis, yAxis;
+    private int jumpBufferCounter = 0;
+    [SerializeField] private int jumpBufferFrames;
+    private float coyoteTimeCounter = 0;
+    [SerializeField] private float coyoteTime;
+    private int airJumpCounter = 0;
+    [SerializeField] private int maxAirJump;
+
+    [Header("Cround Check Setting")]
+    [SerializeField] private float jumpForce = 45;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+
+    [Header("Dash setting")]
+    PlayerStateList pState;
+    private float gravity;
+    private bool canDash;
+    private bool dashed;
+    [SerializeField] GameObject dashEffect;
+
+    [Header("Attack")]
+    bool attack = false;
+    float timeBetweenAttack, timeSinceAtttack;
+    [SerializeField] Transform SideAttackTransform, UpAttackTransform, DownAttackTransform;
+    [SerializeField] Vector2 SideAttackArea, UpAttackArea, DownAttackArea;
+    [SerializeField] LayerMask attackableLayer;
+    private void Awake()
+    {
+       if (Instance != null && Instance != this) 
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+        pState = GetComponent<PlayerStateList>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        gravity = rb.gravityScale;
+    }
+
+    private void nDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(SideAttackTransform.position, SideAttackArea);
+        Gizmos.DrawWireCube(UpAttackTransform.position, UpAttackArea);
+        Gizmos.DrawWireCube(DownAttackTransform.position, DownAttackArea);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        UpdateJumpVariable();
+        if (pState.dashing) return;
+        Flip();
+        Move();
+        Jump();
+        StartDash();
+        Attack();
+    }
+    void GetInputs()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        attack = Input.GetMouseButtonDown(0);
+    }
+    void Flip()
+    {
+        if(xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }else if(xAxis > 0) 
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+    }
+    private void Move()
+    {
+        rb.velocity = new Vector2(speed* xAxis, rb.velocity.y);
+        anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
+        anim.SetBool("Falling", rb.velocity.y < 0 && !Grounded());
+    }
+    void StartDash()
+    {
+        if (Input.GetButtonDown("Dash") && !dashed)
+        {
+            StartCoroutine(Dash());
+            dashed = true;
+        }
+        if(Grounded())
+        {
+            dashed = false;
+        }
+    }
+    IEnumerator Dash()
+    {
+        canDash = false;
+        pState.dashing = true;
+        anim.SetTrigger("Dashing");
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        Vector3 spawnPosition = transform.position;
+        Instantiate(dashEffect, spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(dashTime);
+
+        rb.gravityScale = gravity;
+        pState.dashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+    void Attack()
+    {
+        timeSinceAtttack += Time.deltaTime;
+        if(attack && timeSinceAtttack >= timeBetweenAttack)
+        {
+            timeSinceAtttack = 0;
+            anim.SetTrigger("Attacking");
+        }
+        if(yAxis == 0 || yAxis <0 && Grounded())
+        {
+            Hit(SideAttackTransform, SideAttackArea);
+        }
+        else if(yAxis > 0)
+        {
+            Hit(UpAttackTransform, UpAttackArea);
+        }
+        else if (yAxis < 0 && !Grounded())
+        {
+            Hit(DownAttackTransform, DownAttackArea);
+        }
+    }
+
+    private void Hit(Transform _attackTransform, Vector2 _attackArea)
+    {
+        Collider2D[] objectsToHit = Physics2D.OverlapBoxAll(_attackTransform.position, _attackArea, 0, attackableLayer);
+        if(objectsToHit != null && objectsToHit.Length > 0)
+        {
+            Debug.Log("Hit");
+        }
+    }
+    public bool Grounded()
+    {
+        if(Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX,0,0), Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+            pState.jumping = false;
+        }
+        if (!pState.jumping)
+        {
+            if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+                pState.jumping = true;
+            }
+            else if(!Grounded() && airJumpCounter < maxAirJump && Input.GetButtonDown("Jump"))
+            {
+                pState.jumping = true;
+                airJumpCounter++;
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+            }
+        }
+        anim.SetBool("Jumping", !Grounded());
+    }
+    void UpdateJumpVariable()
+    {
+        if (Grounded())
+        {
+            pState.jumping = false;
+            coyoteTimeCounter = coyoteTime;
+            airJumpCounter = 0;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferFrames;
+        }
+        else
+        {
+            jumpBufferCounter--;
+        }
+    }
+}
